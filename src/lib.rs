2ea3081e194a561e675432a48c43d58ec939ff63
cyanolupus <cyanolupus@gmail.com>
2023-05-01 23:12:47 +0900

    fix clippy code lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ fn log_request(req: &Request) {
         Date::now().to_string(),
         req.path(),
         req.cf().coordinates().unwrap_or_default(),
-        req.cf().region().unwrap_or("unknown region".into())
+        req.cf().region().unwrap_or_else(|| "unknown region".into())
     );
 }
 
@@ -28,7 +28,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
             let host = req.headers().get("host").unwrap_or_default();
             match host {
                 Some(host) => {
-                    let (subdomain, host) = parse_host(host.to_string());
+                    let (subdomain, host) = parse_host(host);
                     let subdomain_punycode = convert_punycode(subdomain);
                     let mut title = format!("{}おわりや", subdomain_punycode);
                     let mut message = format!("{}おわりが売ってる", subdomain_punycode);
@@ -66,7 +66,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
             let host = req.headers().get("host").unwrap_or_default();
             match host {
                 Some(host) => {
-                    let (subdomain, _) = parse_host(host.to_string());
+                    let (subdomain, _) = parse_host(host);
                     let subdomain_punycode = convert_punycode(subdomain);
                     let emoji = owariya_image(subdomain_punycode);
                     let mut emoji_ico = Vec::new();
@@ -82,7 +82,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
             let host = req.headers().get("host").unwrap_or_default();
             match host {
                 Some(host) => {
-                    let (subdomain, _) = parse_host(host.to_string());
+                    let (subdomain, _) = parse_host(host);
                     let subdomain_punycode = convert_punycode(subdomain);
                     let emoji = owariya_image(subdomain_punycode);
                     let mut emoji_png = Vec::new();
@@ -121,18 +121,18 @@ fn owariya_image(subdomain: String) -> image::DynamicImage {
         }
     }
 
-    if subdomain == "" {
-        let owa = "おわ".to_string();
-        let riya = "りや".to_string();
-        let scale_owa = get_scale_by_font(height_f32 / 2.0, width_f32, &font, &owa);
-        let scale_riya = get_scale_by_font(height_f32 / 2.0, width_f32, &font, &riya);
-        draw_text_mut(&mut img, font_color, x, y, scale_owa, &font, &owa);
+    if subdomain.is_empty() {
+        let owa = "おわ";
+        let riya = "りや";
+        let scale_owa = get_scale_by_font(height_f32 / 2.0, width_f32, &font, owa);
+        let scale_riya = get_scale_by_font(height_f32 / 2.0, width_f32, &font, riya);
+        draw_text_mut(&mut img, font_color, x, y, scale_owa, &font, owa);
         y += height / 2;
-        draw_text_mut(&mut img, font_color, x, y, scale_riya, &font, &riya);
+        draw_text_mut(&mut img, font_color, x, y, scale_riya, &font, riya);
     } else {
-        let owariya = "おわりや".to_string();
+        let owariya = "おわりや";
         let scale_subdomain = get_scale_by_font(height_f32 / 2.0, width_f32, &font, &subdomain);
-        let scale_owariya = get_scale_by_font(height_f32 / 2.0, width_f32, &font, &owariya);
+        let scale_owariya = get_scale_by_font(height_f32 / 2.0, width_f32, &font, owariya);
         draw_text_mut(
             &mut img,
             font_color,
@@ -143,13 +143,13 @@ fn owariya_image(subdomain: String) -> image::DynamicImage {
             &subdomain,
         );
         y += height / 2;
-        draw_text_mut(&mut img, font_color, x, y, scale_owariya, &font, &owariya);
+        draw_text_mut(&mut img, font_color, x, y, scale_owariya, &font, owariya);
     }
 
     img
 }
 
-fn get_scale_by_font(height: f32, width: f32, font: &Font, text: &String) -> Scale {
+fn get_scale_by_font(height: f32, width: f32, font: &Font, text: &str) -> Scale {
     let mut glyph_width_sum = 0.0;
     for c in text.chars() {
         let glyph = font.glyph(c).scaled(Scale::uniform(height));
@@ -158,11 +158,10 @@ fn get_scale_by_font(height: f32, width: f32, font: &Font, text: &String) -> Sca
     if glyph_width_sum == 0.0 {
         glyph_width_sum = 1.0;
     }
-    let scale = Scale {
+    Scale {
         x: height * width / glyph_width_sum,
         y: height,
-    };
-    scale
+    }
 }
 
 fn parse_host(host: String) -> (String, String) {
